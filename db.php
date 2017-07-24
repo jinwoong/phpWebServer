ce232f59af154d33d96f28d9b7e2c5c1132f6095
jinwoong <ultraman1128@gmail.com>
2017-07-23 23:04:00 -0400

    added clear db

--- db.php
+++ db.php
@@ -1,9 +1,10 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-$servername = "localhost";
-$username = "root";
-$password = "rktfjqm1.";
-$dbname = "kapcq";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
