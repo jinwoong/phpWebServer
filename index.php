230b74e68d50d7e031444b8e0c2f4f2df92d673d
jinwoong <ultraman1128@gmail.com>
2017-07-23 23:15:30 -0400

    added php debug

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 include "db.php";
 $sql = "SELECT * FROM Members ORDER BY _id DESC";
 $result = $conn->query($sql);
-echo "It works until result";
+echo "It works until result\r\n";
 if ($result->num_rows > 0) {
     // output data of each row
     $data = array();
-    echo "The result count in bigger than 0";
+    echo "The result count in bigger than 0\r\n";
     while($row = $result->fetch_assoc()) {
+        echo $row;
         $data[] = $row;
     }
+    echo $data;
     echo json_encode($data);
 } else {
     echo "0";
