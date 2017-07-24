d0cccb460c13a204433e2a72809e5a9738a4d002
jinwoong <ultraman1128@gmail.com>
2017-07-23 23:37:54 -0400

    Added header on db.php

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
