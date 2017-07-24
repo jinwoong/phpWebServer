fd2f72f8dc45b3ed44922ea3aff49d4cfbfc00db
jinwoong <ultraman1128@gmail.com>
2017-07-24 00:11:35 -0400

    added selectAttendanceRate.php

---
+++ selectAttendanceRate.php
@@ -0,0 +1,21 @@
+<?php
+$data = json_decode(file_get_contents("php://input"));
+include "db.php";
+$sql = "SELECT a.value_date, COUNT(a.status)/(SELECT COUNT(_id) FROM Members) * 100 AS attendance_rate
+        FROM Attendance a
+        WHERE a.status = 'present'
+        GROUP BY a.value_date
+        HAVING a.value_date > '$data->value_date'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    $data = array() ;
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo "0";
+}
+$conn->close();
+?>
